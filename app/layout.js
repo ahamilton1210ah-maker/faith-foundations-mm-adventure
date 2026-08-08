234eb978a8c23876fe82c4456303cea389c3ef9f
ahamilton1210ah-maker <a.hamilton1210.ah@gmail.com>
2026-08-08 10:36:09 -0400

    Add RootLayout component with metadata for app

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Faith Foundations: The M&M Adventure",
+  description: "Every child's faith matters.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
